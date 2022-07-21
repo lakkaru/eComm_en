75ca987c04fce800e93f04c426397314d86b94ce
lakkaru <lakkaru@gmail.com>
2022-07-21 08:08:13 +0530

    jqery added

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,10 +9,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <!-- Boostrap JavaScript Bundle -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <!-- jquery minified -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     
     <title>eComm_en</title>
 </head>
 <body>
+
+    <script>
+        $(document).ready (function(){
+            $("button").click (function(){
+                alert('All Set')
+            })
+        })
+    </script>
     
 </body>
 </html>
